17a2820711a1b99b21cda746642faac7aaf357e7
sebastian93921 <sebastian93921@gmail.com>
2022-06-12 21:11:35 +0800

    Update osed-start-env.sh

--- osed-start-env.sh
+++ osed-start-env.sh
@@ -11,6 +11,7 @@ then
     exit 1
 fi
 
+wget https://github.com/sebastian93921/OSED-Code-Snippets/raw/main/rp-win-x86.exe -O rp-win-x86.exe
 wget https://github.com/sebastian93921/OSED-Code-Snippets/raw/main/generate-shellcode-32.py -O generate-shellcode-32.py
 wget https://github.com/sebastian93921/OSED-Code-Snippets/blob/main/generate-egghunter-32.py -O generate-egghunter-32.py
 wget https://github.com/sebastian93921/OSED-Code-Snippets/raw/main/find-ppr-32.py -O find-ppr-32.py
@@ -20,7 +21,7 @@ wget https://github.com/corelan/windbglib/raw/master/pykd/pykd.zip -O pykd.zip
 
 printf "bitsadmin /Transfer myJob http://$myip:8080/windbg.wew"' C:\\windows\\temp\\windbg.wew \n' > start-win-debugenv.bat
 printf "bitsadmin /Transfer myJob http://$myip:8080/find-ppr-32.py"' C:\\windows\\temp\\find-ppr-32.py \n' >> start-win-debugenv.bat
-printf "bitsadmin /Transfer myJob http://$myip:8080/start-windbg.bat"' .\start-windbg.bat \n' >> start-win-debugenv.bat
+printf "bitsadmin /Transfer myJob http://$myip:8080/rp-win-x86.exe"' .\rp-win-x86.exe \n' >> start-win-debugenv.bat
 printf 'echo "C:\Program Files\Windows Kits\\10\Debuggers\x86\windbg.exe" -WF C:\\windows\\temp\\windbg.wew > WinDbg.bat' >> start-win-debugenv.bat
 
 
